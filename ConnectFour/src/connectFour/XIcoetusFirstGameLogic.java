7aee8f25203f7cec6b16e648caf7c59210ace477
Mikkel <mikkel.funch@gmail.com>
2014-02-28 15:54:50 +0100

    Implemented minimax

--- ConnectFour/src/connectFour/XIcoetusFirstGameLogic.java
+++ ConnectFour/src/connectFour/XIcoetusFirstGameLogic.java
@@ -1,50 +1,115 @@
 package connectFour;
 
+import java.util.List;
+
 public class XIcoetusFirstGameLogic implements IGameLogic {
-    private int x = 0;
-    private int y = 0;
-    private int playerID;
-    private int[][] gameboard;
-    private int[] nextCoinPos;
-    
-    public XIcoetusFirstGameLogic() {
-        //TODO Write your implementation for this method
-    }
-	
-    public void initializeGame(int x, int y, int playerID) {
-        this.x = x;
-        this.y = y;
-        this.playerID = playerID;
-        gameboard = new int[x][y];
-        nextCoinPos = new int[x];
-        //TODO Write your implementation for this method
-    }
-	
-    public Winner gameFinished() {
-        //TODO Write your implementation for this method
-        return Winner.NOT_FINISHED;
-    }
+	private int x = 0;
+	private int y = 0;
+	private int playerID;
+	private int[][] gameBoard;
+	private int[] nextCoinPos;
 
+	public XIcoetusFirstGameLogic() {
+		// TODO Write your implementation for this method
+	}
 
-    public void insertCoin(int column, int playerID) {
-    	gameboard[column][nextCoinPos[column]] = playerID;
-    	nextCoinPos[column]++;
-    	printGameboard();
-    }
+	public void initializeGame(int x, int y, int playerID) {
+		this.x = x;
+		this.y = y;
+		this.playerID = playerID;
+		gameBoard = new int[x][y];
+		nextCoinPos = new int[x];
+		// TODO Write your implementation for this method
+	}
 
-    public int decideNextMove() {
-        //TODO Write your implementation for this method
-        return 0;
-    }
-    
-    private void printGameboard() {
-    	for(int i = y-1; 0 <= i; i--) {
-    		for(int j = 0; j < x; j++) {
-    			System.out.print(gameboard[j][i] + " ");
+	public Winner gameFinished() {
+		// TODO Write your implementation for this method
+		return Winner.NOT_FINISHED;
+	}
+
+	public void insertCoin(int column, int playerID) {
+		gameBoard[column][nextCoinPos[column]] = playerID;
+		nextCoinPos[column]++;
+		printGameboard();
+	}
+
+	/**
+	 * 
+	 * @return The column in which it is best to place the coin
+	 */
+	public int decideNextMove() {
+		Action bestAction = null;
+		// AI is blue who wants to maximize the utility
+		List<Action> actions = Action.getActions(x, y, playerID, gameBoard);
+		for (Action a : actions) {
+			gameBoard = a.apply(gameBoard);
+			if (playerID == 1) {
+				maxValue(a);
+			} else {
+				minValue(a);
+			}
+			if (bestAction == null || bestAction.getUtility() > a.getUtility()) {
+				bestAction = a;
+			}
+			gameBoard = a.undo(gameBoard);
+		}
+		return bestAction.getColumn();
+	}
+
+	private void minValue(Action appliedAction) {
+		if(gameFinished() == Winner.NOT_FINISHED){
+		Action bestAction = null;
+		
+		List<Action> actions = Action.getActions(x, y, 1, gameBoard);
+		for (Action a : actions) {
+			gameBoard = a.apply(gameBoard);
+			minValue(a);
+			if (bestAction == null || bestAction.getUtility() < a.getUtility()) {
+				bestAction = a;
+			}
+			gameBoard = a.undo(gameBoard);
+		}
+		
+	} else if(gameFinished() == Winner.PLAYER1) {
+		appliedAction.setUtility(1d);;
+	} else if(gameFinished() == Winner.PLAYER2) {
+		appliedAction.setUtility(-1d);
+	} else {
+		appliedAction.setUtility(0d);
+	}
+	}
+
+	public void maxValue(Action appliedAction){
+		if(gameFinished() == Winner.NOT_FINISHED){
+    		Action bestAction = null;
+    		
+    		List<Action> actions = Action.getActions(x, y, 1, gameBoard);
+    		for (Action a : actions) {
+    			gameBoard = a.apply(gameBoard);
+    			maxValue(a);
+				if (bestAction == null || bestAction.getUtility() < a.getUtility()) {
+					bestAction = a;
+				}
+				gameBoard = a.undo(gameBoard);
     		}
-    		System.out.println();
+    		
+    	} else if(gameFinished() == Winner.PLAYER1) {
+    		appliedAction.setUtility(1d);;
+    	} else if(gameFinished() == Winner.PLAYER2) {
+    		appliedAction.setUtility(-1d);
+    	} else {
+    		appliedAction.setUtility(0d);
     	}
-		System.out.println();
     }
+
+	private void printGameboard() {
+		for (int i = y - 1; 0 <= i; i--) {
+			for (int j = 0; j < x; j++) {
+				System.out.print(gameBoard[j][i] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 
 }
